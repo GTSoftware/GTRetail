54ee97cd2a089ef11f40834e6764028b4238a1b1
rodrigo <rodrigo@rotatomel.dyndns-web.com>
2014-08-18 15:30:18 -0300

    Comandos para dump

---
+++ GTRetail-ejb/src/main/resources/META-INF/Comandos.sh
@@ -0,0 +1,2 @@
+#Dump postgres DB
+pg_dump --host localhost --port 5432 --username "retail" --role "retail" -W --format plain --blobs --encoding UTF8 --create --clean > NetBeansProjects/GTRetail-MVN/GTRetail-ejb/src/main/resources/META-INF/DDL.sql
