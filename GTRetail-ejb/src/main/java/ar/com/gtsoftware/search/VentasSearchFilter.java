d444441ba0b968e1029c9693d398de5251d3a9f4
rodrigo <rodrigo@rodrigo-laptop-acer-local>
2014-08-28 21:34:46 -0300

    Search filter de ventas

---
+++ GTRetail-ejb/src/main/java/ar/com/gtsoftware/search/VentasSearchFilter.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2014 GT Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.gtsoftware.search;
+
+import ar.com.gtsoftware.model.NegocioCondicionesOperaciones;
+import ar.com.gtsoftware.model.Personas;
+import ar.com.gtsoftware.model.Sucursales;
+import ar.com.gtsoftware.model.Usuarios;
+import java.util.Date;
+
+/**
+ *
+ * @author Rodrigo Tato <rotatomel@gmail.com>
+ */
+public class VentasSearchFilter extends SearchFilter {
+
+    private Integer idVenta;
+    private Date fechaVentaDesde;
+    private Date fechaVentaHasta;
+    private Boolean anulada;
+    private Usuarios idUsuario;
+    private Personas idPersona;
+    private Sucursales idSucursal;
+    private Boolean facturada;
+    private NegocioCondicionesOperaciones condicionVenta;
+
+    @Override
+    public boolean hasFilter() {
+        return (idVenta != null) || (fechaVentaDesde != null) || (fechaVentaHasta != null)
+                || (anulada != null) || (idUsuario != null)
+                || (idSucursal != null) || (idPersona != null)
+                || (facturada != null) || (condicionVenta != null);
+    }
+
+    public VentasSearchFilter() {
+    }
+
+    public VentasSearchFilter(Date fechaDesde, Date fechaHasta, Boolean anulada) {
+        this.fechaVentaDesde = fechaDesde;
+        this.fechaVentaHasta = fechaHasta;
+        this.anulada = anulada;
+    }
+
+    public Integer getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Boolean isAnulada() {
+        return anulada;
+    }
+
+    public void setAnulada(Boolean anulada) {
+        this.anulada = anulada;
+    }
+
+    public Usuarios getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Usuarios idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Personas getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Personas idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public Sucursales getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(Sucursales idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public Date getFechaVentaDesde() {
+        return fechaVentaDesde;
+    }
+
+    public void setFechaVentaDesde(Date fechaVentaDesde) {
+        this.fechaVentaDesde = fechaVentaDesde;
+    }
+
+    public Date getFechaVentaHasta() {
+        return fechaVentaHasta;
+    }
+
+    public void setFechaVentaHasta(Date fechaVentaHasta) {
+        this.fechaVentaHasta = fechaVentaHasta;
+    }
+
+    public Boolean isFacturada() {
+        return facturada;
+    }
+
+    public void setFacturada(Boolean facturada) {
+        this.facturada = facturada;
+    }
+
+    public NegocioCondicionesOperaciones getCondicionVenta() {
+        return condicionVenta;
+    }
+
+    public void setCondicionVenta(NegocioCondicionesOperaciones condicionVenta) {
+        this.condicionVenta = condicionVenta;
+    }
+
+}
