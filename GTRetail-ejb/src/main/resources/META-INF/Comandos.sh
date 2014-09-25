1990bb57a2ba466763f22dab2ca18afbccdfdc24
rodrigo <rotatomel@gmail.com>
2014-09-25 01:31:06 -0300

    Comandos

--- GTRetail-ejb/src/main/resources/META-INF/Comandos.sh
+++ GTRetail-ejb/src/main/resources/META-INF/Comandos.sh
@@ -2,7 +2,7 @@
 pg_dump --host localhost --port 5432 --username "retail" --role "retail" -W --format plain --blobs --encoding UTF8 --create --clean > NetBeansProjects/GTRetail-MVN/GTRetail-ejb/src/main/resources/META-INF/DDL.sql
 
 #Dump data
-pg_dump --column-inserts --data-only --host localhost --port 5432 --username "retail" --role "retail" -W > NetBeans\ Projects/GTRetail-MVN/GTRetail-ejb/src/main/resources/META-INF/Data.sql
+pg_dump --column-inserts --data-only --host localhost --port 5432 --username "retail" --role "retail" -W > NetBeansProjects/GTRetail-MVN/GTRetail-ejb/src/main/resources/META-INF/Data.sql
 
 #Dump for PGDiff
 pg_dump -h prod.server.com -s -f original.sql cool-app-db
