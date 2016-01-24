633021807fb89faef50931876940216e627080d2
rodrigo <rotatomel@gmail.com>
2016-01-24 00:32:42 -0300

    Agregado de columnas

--- GTRetail-ejb/src/main/resources/db/migration/V1.7__Productos_Datos.sql
+++ GTRetail-ejb/src/main/resources/db/migration/V1.7__Productos_Datos.sql
@@ -6,3 +6,9 @@ add column stock_minimo numeric(19,2) NOT NULL default 0;
 
 alter table productos
 add column stock_actual numeric(19,2) NOT NULL default 0;
+
+alter table productos
+add constraint unique_codigo_propio unique(codigo_propio);
+
+alter table productos
+add column codigo_fabricante varchar(60);
